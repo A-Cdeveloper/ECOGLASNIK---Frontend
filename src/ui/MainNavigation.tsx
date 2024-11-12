f629b0b72387951c6eefeac7c4b8567560a694b9
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-13 00:35:33 +0100

    reorder main menu Add link to user personal problems

--- src/ui/MainNavigation.tsx
+++ src/ui/MainNavigation.tsx
@@ -1,21 +1,26 @@
 import { NavLink } from "react-router-dom";
 
-const MainNavigation = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
+const MainNavigation = ({
+  isAuthenticated,
+  userId,
+}: {
+  isAuthenticated: boolean;
+  userId?: number;
+}) => {
   return (
     <nav className="flex-1 order-2 lg:order-1 w-full pt-3 pb-2">
       <ul className="flex justify-center lg:justify-end uppercase gap-5 font-bold pe-4">
-        {!isAuthenticated && (
-          <li>
-            <NavLink
-              to="/problems/add"
-              className={({ isActive }) =>
-                isActive ? "text-winter" : "text-secondary hover:text-winter"
-              }
-            >
-              Uloguj se
-            </NavLink>
-          </li>
-        )}
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-winter" : "text-secondary hover:text-winter"
+            }
+          >
+            Svi problemi
+          </NavLink>
+        </li>
+
         {isAuthenticated && (
           <li>
             <NavLink
@@ -28,16 +33,42 @@ const MainNavigation = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
             </NavLink>
           </li>
         )}
+        {isAuthenticated && (
+          <li>
+            <NavLink
+              to={`/problems/user/${userId}`}
+              className={({ isActive }) =>
+                isActive ? "text-winter" : "text-secondary hover:text-winter"
+              }
+            >
+              Vaše prijave
+            </NavLink>
+          </li>
+        )}
         <li>
           <NavLink
             to="/impressum"
             className={({ isActive }) =>
-              isActive ? "text-winter" : "text-secondary hover:text-winter"
+              isActive
+                ? "ms-0 text-winter"
+                : "ms-0  text-secondary hover:text-winter"
             }
           >
             Impressum
           </NavLink>
         </li>
+        {!isAuthenticated && (
+          <li>
+            <NavLink
+              to="/login/?mode=login"
+              className={({ isActive }) =>
+                isActive ? "text-winter" : "text-secondary hover:text-winter"
+              }
+            >
+              Uloguj se
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
