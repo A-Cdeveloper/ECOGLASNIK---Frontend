d4d014a2b9e706f97369bc97ed3a24d2748aedd1
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-12 01:32:57 +0100

    add user to form component

--- src/features/problems/ProblemForm.tsx
+++ src/features/problems/ProblemForm.tsx
@@ -1,11 +1,13 @@
+import useAuth from "../../context/useAuth";
 import Form from "./forms/Form";
 
 const ProblemForm = ({ problemId }: { problemId?: string }) => {
   const editMode = problemId ? true : false;
+  const { user } = useAuth();
 
   return (
     <>
-      <Form editMode={editMode} problemId={problemId} />
+      <Form editMode={editMode} problemId={problemId} user={user} />
     </>
   );
 };
