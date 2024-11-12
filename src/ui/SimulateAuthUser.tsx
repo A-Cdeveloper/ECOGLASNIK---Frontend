d0e4a4d83e8573f96e8523c20e7f5284c48dca9e
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-12 01:39:19 +0100

    simulate user login Add another user

--- src/ui/SimulateAuthUser.tsx
+++ src/ui/SimulateAuthUser.tsx
@@ -32,7 +32,27 @@ const SimulateAuthUser = () => {
           })
         }
       >
-        Login User
+        Login User Aleksandar
+      </button>
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          setSessionStorageData({
+            user: {
+              uid: 2,
+              firstname: "Biljana",
+              lastname: "Vuckovic",
+              phone: "123456789",
+              email: "biljana.vuckovic@gmailcom",
+              password: "123456789",
+              accessToken: "123456789",
+              refreshToken: "123456789",
+              createdAt: new Date(),
+            },
+          })
+        }
+      >
+        Login User Biljana
       </button>
       <button className="btn btn-primary" onClick={removeSessionStorageData}>
         Logoff User
