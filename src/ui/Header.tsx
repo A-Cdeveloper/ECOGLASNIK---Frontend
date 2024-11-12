6952c079aaa02bed5ffca7644b0c31580c1919a1
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-13 00:36:05 +0100

    get current user id in Header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header className="w-full h-auto py-0 px-2 border-b-3 border-secondary/50 flex flex-wrap items-center justify-between bg-secondary/30 gap-x-0 md-gap-x-6">
       <Logo />
-      <MainNavigation isAuthenticated={isAuthenticated} />
+      <MainNavigation isAuthenticated={isAuthenticated} userId={user?.uid} />
       <SimulateAuthUser />
       {isAuthenticated && (
         <UserNavigation user={user} logout={removeSessionStorageData} />
