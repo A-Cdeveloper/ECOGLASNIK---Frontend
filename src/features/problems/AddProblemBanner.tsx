8597c8b651f69fa63564b12aa64dc799420c94c5
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-17 14:47:00 +0100

    change bgcolor AddProblemBanner

--- src/features/problems/AddProblemBanner.tsx
+++ src/features/problems/AddProblemBanner.tsx
@@ -1,6 +1,6 @@
 const AddProblemBanner = () => {
   return (
-    <div className=" bg-yellow/50 px-2 py-2 text-center text-[15px]/[1.25]">
+    <div className=" bg-yellow/80 px-2 py-2 text-center text-[15px]/[1.25]">
       Klinite na mapu kako biste odabrali lokaciju problema koji prijavljujete
       ili na marker da biste videli detalje već prijavljenog problema.
     </div>
