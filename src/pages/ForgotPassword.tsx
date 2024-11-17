88916fdc4ec166a4a8593d5d23f9043dfe34f4a2
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-17 14:46:43 +0100

    add back button  ForgotPassword component

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import useAuth from "../context/useAuth";
 import ForgotPasswordForm from "../features/auth/ForgotPasswordForm";
+import BackButton from "../ui/Buttons/BackButton";
 
 const ForgotPassword = () => {
   const { isAuthenticated } = useAuth();
@@ -10,9 +11,12 @@ const ForgotPassword = () => {
     return <Navigate to="/" replace />;
   }
   return (
-    <div className="w-full h-[80%] flex flex-col items-center justify-center px-9">
-      <ForgotPasswordForm />
-    </div>
+    <>
+      <BackButton to={-1} />
+      <div className="w-full h-[80%] flex flex-col items-center justify-center px-9">
+        <ForgotPasswordForm />
+      </div>
+    </>
   );
 };
 
