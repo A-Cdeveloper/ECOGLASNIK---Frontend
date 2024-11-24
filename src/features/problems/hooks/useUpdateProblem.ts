7153f36a7c84198c88f68d1b24626f8e50a15f37
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-24 02:31:28 +0100

    update problem status refetch problems

--- src/features/problems/hooks/useUpdateProblem.ts
+++ src/features/problems/hooks/useUpdateProblem.ts
@@ -14,7 +14,8 @@ const useUpdateProblem = (): UseMutationResult<Problem, Error, Problem> => {
     mutationFn: updateProblemApi,
     onSuccess: () => {
       toast.success("Problem uspešno promenjen!");
-      queryClient.invalidateQueries({ queryKey: ["problem"] }); // Optional: to refresh the problems query
+      queryClient.invalidateQueries({ queryKey: ["problem"] });
+      queryClient.invalidateQueries({ queryKey: ["problems"] }); // Optional: to refresh the problems query
     },
     onError: () => {
       toast.error("Došlo je do greške pri izmeni detalja problema.");
