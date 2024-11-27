7079ec6cbb58dc6c78eb349fc02b329d265c23cf
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-27 11:37:58 +0100

    define final map bound

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
-export const INITIAL_ZOOM = 13;
+export const INITIAL_ZOOM = 12.5;
 export const DEFAULT_POSITION = { lat: 42.961498, lng: 22.124319 };
 export const DEFAULT_BOUND = {
-  northEast: { lat: 43.001498, lng: 22.164319 },
-  southWest: { lat: 42.921498, lng: 22.084319 },
+  northEast: { lat: 43.028808, lng: 22.369252 },
+  southWest: { lat: 42.887599, lng: 22.039992 },
 };
 
 export const API_URL = import.meta.env.VITE_API_URL;
