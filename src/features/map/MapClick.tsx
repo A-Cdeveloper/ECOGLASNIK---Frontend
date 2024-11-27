9f863fbf24724054ea259376fda6f612e2059cd9
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-27 12:43:49 +0100

    click on map out of range location

--- src/features/map/MapClick.tsx
+++ src/features/map/MapClick.tsx
@@ -1,13 +1,31 @@
+import L from "leaflet";
 import { useMapEvent } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
-
-const MapClick = () => {
+import { DEFAULT_BOUND } from "../../constants";
+const MapClick = ({
+  onClickOutRange,
+}: {
+  onClickOutRange: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   const navigate = useNavigate();
+
+  // Define the bounds
+  const bounds = L.latLngBounds(
+    L.latLng(DEFAULT_BOUND.southWest),
+    L.latLng(DEFAULT_BOUND.northEast)
+  );
 
   useMapEvent("click", (e) => {
     const { lat, lng } = e.latlng;
 
     if (!lat || !lng) return;
+
+    // Check if the click is within bounds
+    if (!bounds.contains(e.latlng)) {
+      onClickOutRange(true);
+      console.log("out of range");
+      return;
+    }
 
     navigate(`problems/add?lat=${lat}&lng=${lng}`);
   });
