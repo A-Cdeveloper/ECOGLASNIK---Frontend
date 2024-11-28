26e8f6e5ac3a941aefe2a551a7e075f07ddb660a
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-28 01:46:57 +0100

    starter version

---
+++ src/features/problems/hooks/useAddEditProblem.ts
@@ -0,0 +1,28 @@
+import {
+  UseMutationResult,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { Problem } from "../../../types";
+import { addNewProblemApi } from "../api/problems";
+
+const useAddNewProblem = (): UseMutationResult<Problem, Error, Problem> => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<Problem, Error, Problem>({
+    mutationFn: addNewProblemApi,
+    onSuccess: () => {
+      toast.success("Problem uspešno dodat!");
+      queryClient.invalidateQueries({ queryKey: ["problem"] });
+      queryClient.invalidateQueries({ queryKey: ["problems"] }); // Optional: to refresh the problems query
+    },
+    onError: () => {
+      toast.error("Došlo je do greške pri dodavanju problema.");
+    },
+  });
+
+  return mutation;
+};
+
+export default useAddNewProblem;
