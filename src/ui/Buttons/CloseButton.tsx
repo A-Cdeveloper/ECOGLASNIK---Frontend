e46e4052384a434b31a61b8a7869dc0e245cbdf5
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-28 02:26:09 +0100

    change position of close button

--- src/ui/Buttons/CloseButton.tsx
+++ src/ui/Buttons/CloseButton.tsx
@@ -1,7 +1,7 @@
 const CloseButton = ({ onClick }: { onClick: () => void }) => {
   return (
     <span
-      className="block bg-red text-white w-5 h-5 absolute top-2 right-0 cursor-pointer rounded-full text-center text-[20px]/[1.25] font-bold"
+      className="block bg-red text-white w-5 h-5 absolute -top-2 right-0 cursor-pointer rounded-full text-center text-[20px]/[1.25] font-bold"
       onClick={onClick}
     >
       &times;
