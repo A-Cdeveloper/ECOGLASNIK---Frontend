51d9ac6235e33793e59dfff98388af257a14071c
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-28 02:02:16 +0100

    combine add /edit problem hook into one hook

--- src/features/problems/hooks/useAddEditProblem.ts
+++ src/features/problems/hooks/useAddEditProblem.ts
@@ -5,24 +5,32 @@ import {
 } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { Problem } from "../../../types";
-import { addNewProblemApi } from "../api/problems";
+import { addNewProblemApi, updateProblemApi } from "../api/problems";
 
-const useAddNewProblem = (): UseMutationResult<Problem, Error, Problem> => {
+const useAddEditProblem = (
+  mode: string
+): UseMutationResult<Problem, Error, Problem> => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<Problem, Error, Problem>({
-    mutationFn: addNewProblemApi,
+    mutationFn: mode === "add" ? addNewProblemApi : updateProblemApi,
     onSuccess: () => {
-      toast.success("Problem uspešno dodat!");
+      toast.success(
+        `Problem uspešno ${mode === "add" ? "dodat" : "promenjen"}!`
+      );
       queryClient.invalidateQueries({ queryKey: ["problem"] });
       queryClient.invalidateQueries({ queryKey: ["problems"] }); // Optional: to refresh the problems query
     },
     onError: () => {
-      toast.error("Došlo je do greške pri dodavanju problema.");
+      toast.error(
+        `Došlo je do greške pri ${
+          mode === "add" ? "dodavanju" : "izmeni"
+        } problema.`
+      );
     },
   });
 
   return mutation;
 };
 
-export default useAddNewProblem;
+export default useAddEditProblem;
