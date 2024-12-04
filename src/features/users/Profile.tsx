e589746a7c1133ef1e6bd466fb256347d749bfc2
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-04 09:35:23 +0100

    create Profile component

---
+++ src/features/users/Profile.tsx
@@ -0,0 +1,61 @@
+import { useMemo } from "react";
+import useAuth from "../../context/useAuth";
+import Button from "../../ui/Buttons/Button";
+import { formattedDate } from "../../utils/helpers";
+import { useProblems } from "../problems/hooks/useProblems";
+import useDeleteUser from "./hooks/useDeleteUser";
+
+const Profile = () => {
+  const { user } = useAuth();
+  const { problems } = useProblems();
+  const { mutate: deleteUserMutation, status: deleteUserStatus } =
+    useDeleteUser();
+
+  const isLoadingDeleteUser = deleteUserStatus === "pending";
+
+  const userProblemsNumber = useMemo(() => {
+    if (user) {
+      return problems?.filter((problem) => problem.uid === user.uid).length;
+    }
+    return;
+  }, [problems, user]);
+
+  console.log(userProblemsNumber);
+
+  return (
+    <>
+      <div className=" bg-secondary/30 my-4 py-2 px-3 grid grid-cols-[max-content_1fr] gap-y-[7px] gap-x-8 text-[14px] rounded-md">
+        <div>Ime i prezime:</div>
+        <div className="font-bold">
+          {user?.firstname + " " + user?.lastname}
+        </div>
+
+        <div>Telefon:</div>
+        <div>{user?.phone ? user?.phone : "/"}</div>
+
+        <div>Email:</div>
+        <div>{user?.email}</div>
+
+        <div>Role:</div>
+        <div>{user?.role}</div>
+
+        <div>Nalog kreiran:</div>
+        <div>{user?.createdAt && formattedDate(user?.createdAt)}</div>
+
+        <div>Posednja aktivnost:</div>
+        <div>{user?.updatedAt && formattedDate(user?.updatedAt)}</div>
+      </div>
+      <div className="flex justify-end">
+        <Button
+          variation="danger"
+          size="small"
+          onClick={() => deleteUserMutation(Number(user?.uid))}
+        >
+          {isLoadingDeleteUser ? "Brisanje..." : "Obriši nalog 🎈"}
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
