628a7d16a43c7b462b21f87a9c828ae9e68e87de
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-05 19:45:08 +0100

    change notification position

--- src/ui/PromtsAndNotifications/Notifications.tsx
+++ src/ui/PromtsAndNotifications/Notifications.tsx
@@ -3,16 +3,16 @@ import { Toaster } from "react-hot-toast";
 const Notifications = () => {
   return (
     <Toaster
-      position="bottom-center"
+      position="bottom-right"
       gutter={3}
       containerStyle={{
-        right: 0,
+        right: 10,
         top: 10,
       }}
       toastOptions={{
         className: "toaststyle",
         success: {
-          duration: 4000,
+          duration: 3000,
         },
         error: {
           duration: 4000,
