2779f8f4091bd3382c683f2ae8f6bf1115b8f621
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-06 09:19:27 +0100

    remove add problem baner from homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,3 @@
-import AddProblemBanner from "../features/problems/AddProblemBanner";
 import ListProblems from "../features/problems/ListProblems";
 import FilterCategory from "../ui/Filters/FilterCategory";
 import FilterStatus from "../ui/Filters/FilterStatus";
@@ -6,7 +5,6 @@ import FilterStatus from "../ui/Filters/FilterStatus";
 const Homepage = () => {
   return (
     <>
-      <AddProblemBanner />
       <div className="flex flex-wrap border-t border-b border-secondary-500/40 py-2 mt-2">
         <FilterStatus />
         <FilterCategory />
