5d753c664670f5aef2e9e7f1db8cbb7f27030ce5
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-06 15:19:53 +0100

    clear addproblembaner comp

--- src/features/problems/AddProblemBanner.tsx
+++ src/features/problems/AddProblemBanner.tsx
@@ -1,8 +1,10 @@
 const AddProblemBanner = () => {
   return (
-    <div className=" bg-yellow-500/80 px-2 py-2 text-center text-[15px]/[1.25]">
-      Klinite na mapu kako biste odabrali lokaciju problema koji prijavljujete
-      ili na marker da biste videli detalje već prijavljenog problema.
+    <div className="flex flex-col items-center  h-screen space-y-5">
+      <div className=" bg-yellow-500/80 px-2 py-2 text-center text-[15px]/[1.25]">
+        Klinite na mapu kako biste odabrali lokaciju problema koji prijavljujete
+        ili odaberite Vašu trenutnu lokaciju.
+      </div>
     </div>
   );
 };
