d358cf3608c3f6f739f017f52d2143b157642c41
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-07 23:52:03 +0100

    create WToogleButton component

---
+++ src/features/weather/WToogleButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WToogleButton = ({
+  onToogleWeater,
+  children,
+}: {
+  onToogleWeater: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <span
+      className="bg-turquoise-900 absolute top-[-28px] left-[15px] px-2 py-[4px] cursor-pointer text-[13px]"
+      onClick={onToogleWeater}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default WToogleButton;
--- src/features/weather/Weather.tsx
+++ src/features/weather/Weather.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { WeatherApiResponse } from "../../types";
 import Error from "../../ui/Error";
 import MiniSpinner from "../../ui/MiniSpinner";
 import CurrentWeather from "./CurrentWeather";
 import ForecastWeather from "./ForecastWeather";
 import { useWeather } from "./hooks/useWeather";
+import WToogleButton from "./WToogleButton";
 
 const Weather = () => {
   const { data, isLoading, error } = useWeather();
+  const [toogleWeather, setToggleWeather] = useState(false);
 
   if (isLoading) return <MiniSpinner />;
   if (error) return <Error message="Došlo je do greške" />;
@@ -14,9 +17,17 @@ const Weather = () => {
   const { current, forecast } = data as Omit<WeatherApiResponse, "location">;
 
   return (
-    <div className="w-full h-auto absolute left-0 bottom-0 lg:bottom-[65px] py-2 px-4 z-[401] bg-primary-500 flex flex-wrap gap-x-3 gap-y-1 items-center">
-      <CurrentWeather todayWeather={current} />
-      <ForecastWeather forecastWeather={forecast} />
+    <div className="w-full h-auto absolute left-0 bottom-0 lg:bottom-[65px] py-2 px-4 z-[99999] bg-primary-500">
+      <WToogleButton onToogleWeater={() => setToggleWeather(!toogleWeather)}>
+        Vremenska prognoza {toogleWeather ? "▼" : "▲"}
+      </WToogleButton>
+
+      {toogleWeather && (
+        <div className="flex flex-wrap gap-x-3 gap-y-1 items-center">
+          <CurrentWeather todayWeather={current} />
+          <ForecastWeather forecastWeather={forecast} />
+        </div>
+      )}
     </div>
   );
 };
