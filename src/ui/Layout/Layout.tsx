4771ae09cdadfb3963ab25b41ac8d46249019cfd
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-07 18:45:48 +0100

    increasize map height on mobile devices

--- src/ui/Layout/Layout.tsx
+++ src/ui/Layout/Layout.tsx
@@ -13,7 +13,7 @@ const Layout = () => {
     <div className="container bg-primary max-w-full h-auto lg:h-screen flex flex-wrap items-start overflow-hidden">
       <Header />
       <Sidebar />
-      <main className="flex-1 h-[350px] lg:h-screen p-3 lg:p-3 mt-[60px] lg:mt-0 order-1 lg:order-2 overflow-hidden relative">
+      <main className="flex-1 h-[450px] lg:h-screen p-3 lg:p-3 mt-[60px] lg:mt-0 order-1 lg:order-2 overflow-hidden relative">
         <Suspense fallback={<div>Loading map...</div>}>
           <MapComponent
             problemId={params.id ? params.id : ""}
