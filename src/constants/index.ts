163e80904cf5b61ecca52d8db9a04e4d0f5f5ce4
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-07 02:05:25 +0100

    add weather api constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,3 +14,7 @@ export const DEFAULT_BOUND = {
   northEast: { lat: 43.028808, lng: 22.369252 },
   southWest: { lat: 42.887599, lng: 22.039992 },
 };
+
+// Weather
+export const WEATHER_BASE_URL = import.meta.env.VITE_WEATHER_BASE_URL;
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
