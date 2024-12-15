06d90ba7b097de441868874a8024de8ae6fc45b8
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-15 02:12:01 +0100

    fix layout scroll bug

--- src/ui/Layout/Layout.tsx
+++ src/ui/Layout/Layout.tsx
@@ -10,11 +10,11 @@ const Layout = () => {
   const params = useParams();
 
   return (
-    <div className="container bg-primary max-w-full h-screen lg:h-screen flex flex-wrap items-start overflow-hidden">
+    <div className="container bg-primary max-w-full h-full lg:h-screen flex flex-wrap items-start overflow-hidden ">
       <Header />
       <Sidebar />
 
-      <main className="flex-1 h-[450px] lg:h-screen pt-[65px] lg:pt-[80px] px-3 lg:px-3 pb-1 lg:pb-5 flex flex-col justify-center order-1 lg:order-2 overflow-hidden relative">
+      <main className="flex-1 h-[400px] lg:h-screen pt-[65px] lg:pt-[80px] px-3 lg:px-3 pb-1 lg:pb-5 flex flex-col justify-center order-1 lg:order-2 overflow-hidden relative">
         <Suspense fallback={<div>Loading map...</div>}>
           <MapComponent
             problemId={params.id ? params.id : ""}
