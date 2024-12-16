53899c3188c9244ec88b7247c7f9db9dd38b66dc
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-16 01:29:19 +0100

    remove navigate from delte problem hook

--- src/features/problems/hooks/useDeleteProblem.ts
+++ src/features/problems/hooks/useDeleteProblem.ts
@@ -6,18 +6,15 @@ import {
 import toast from "react-hot-toast";
 import { deleteProblemApi } from "../api/problems";
 
-import { useNavigate } from "react-router-dom";
 import { Problem } from "../../../types";
 
 const useDeleteProblem = (): UseMutationResult<Problem, Error, string> => {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const mutation = useMutation<Problem, Error, string>({
     mutationFn: deleteProblemApi, // This is the function to delete the problem
     onSuccess: (data: Problem) => {
       toast.success(`Problem "${data.title}" je uklonjen!`);
-      navigate("/"); // Navigate to home page after deletion
       queryClient.invalidateQueries({ queryKey: ["problems"] }); // Optional: refresh the problems query
     },
     onError: (err: Error) => {
