78b26a068f0b93782715f245b8a4eea58d9cfa5d
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-17 03:28:32 +0100

    remove app constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,14 +10,6 @@ export const APP_NAME = "Clean Me";
 // Forms
 export const TEXTAREA_MAX_CHARACTERS = 300;
 
-// Map
-// export const INITIAL_ZOOM = 13;
-// export const DEFAULT_POSITION = { lat: 42.961498, lng: 22.124319 };
-export const DEFAULT_BOUND = {
-  northEast: { lat: 43.028808, lng: 22.369252 },
-  southWest: { lat: 42.887599, lng: 22.039992 },
-};
-
 // Weather
 export const WEATHER_BASE_URL = import.meta.env.VITE_WEATHER_BASE_URL;
 export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
