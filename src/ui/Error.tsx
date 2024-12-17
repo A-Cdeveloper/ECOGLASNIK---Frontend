fb2501d050cee8ff71342772e776ad2eee23a489
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-18 00:25:23 +0100

    add extra prop className to Error component

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,10 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Buttons/Button";
 
-const Error = ({ message }: { message: string }) => {
+const Error = ({
+  message,
+  className,
+}: {
+  message: string;
+  className?: string;
+}) => {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-auto lg:h-[500px] flex flex-col items-center justify-start lg:justify-center mt-5 lg:mt-0">
+    <div
+      className={`w-full h-auto lg:h-[500px] flex flex-col items-center justify-start lg:justify-center mt-5 lg:mt-0 ${className}`}
+    >
       <span className="text-center font-regular text-[20px] block w-full mb-3">
         {message}
       </span>
