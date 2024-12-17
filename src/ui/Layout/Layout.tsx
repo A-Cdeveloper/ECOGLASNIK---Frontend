86703d57ef1c4b79d6dfa5917edc3f9813d45545
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-18 00:31:15 +0100

    Layout component - get app settings at start Load conditionaly base on settings

--- src/ui/Layout/Layout.tsx
+++ src/ui/Layout/Layout.tsx
@@ -1,6 +1,11 @@
-import { lazy, Suspense } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Weather from "../../features/weather/Weather";
+import { useCtxMap } from "../../context/mapContext";
+
+import { useSettings } from "../../features/settings/hooks/useSettings";
+import Error from "../Error";
+import Loader from "../Loader";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
@@ -8,21 +13,55 @@ const MapComponent = lazy(() => import("../../features/map/Map"));
 
 const Layout = () => {
   const params = useParams();
+  const { settings, isLoading, error } = useSettings();
+  const { setMapPosition, setZoomLevel, setDefaultBounds } = useCtxMap();
 
+  useEffect(() => {
+    if (!settings) return;
+    // Update the context
+    setMapPosition(settings.data.defaultPosition);
+    setZoomLevel(settings.data.initialZoom);
+    setDefaultBounds(settings.data.defaultBound);
+  }, [
+    isLoading,
+    error,
+    settings,
+    setMapPosition,
+    setZoomLevel,
+    setDefaultBounds,
+  ]);
+
+  let mainContent;
+
+  if (error) {
+    mainContent = <Error message={error.message} className="!h-full" />;
+  }
+  if (isLoading) {
+    mainContent = <Loader />;
+  }
+
+  if (!isLoading && !error) {
+    mainContent = (
+      <>
+        <Header />
+        <Sidebar />
+
+        <main className="flex-1 h-[400px] lg:h-screen pt-[75px] md:pt-[90px] px-3 lg:px-3 pb-1 lg:pb-5 flex flex-col justify-center order-1 lg:order-2 overflow-hidden relative">
+          <Suspense fallback={<div>Loading map...</div>}>
+            <MapComponent
+              problemId={params.id ? params.id : ""}
+              userId={params.id ? params.id : ""}
+            />
+          </Suspense>
+
+          <Weather />
+        </main>
+      </>
+    );
+  }
   return (
     <div className="container bg-primary max-w-full h-full lg:h-screen flex flex-wrap items-start overflow-hidden ">
-      <Header />
-      <Sidebar />
-
-      <main className="flex-1 h-[400px] lg:h-screen pt-[75px] md:pt-[90px] px-3 lg:px-3 pb-1 lg:pb-5 flex flex-col justify-center order-1 lg:order-2 overflow-hidden relative">
-        <Suspense fallback={<div>Loading map...</div>}>
-          <MapComponent
-            problemId={params.id ? params.id : ""}
-            userId={params.id ? params.id : ""}
-          />
-        </Suspense>
-        <Weather />
-      </main>
+      {mainContent}
     </div>
   );
 };
