6ec89d45eebcfa01f012be51472cd4cb15830b2c
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-18 00:28:43 +0100

    get cachedSettings from query

--- src/features/map/StabilizeMap.tsx
+++ src/features/map/StabilizeMap.tsx
@@ -1,22 +1,28 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import { useCtxMap } from "../../context/mapContext";
-import { useSettings } from "../settings/hooks/useSettings";
+import { SettingsType } from "../../types";
 
 const StabilizeMap = () => {
   const map = useMap();
   const { setZoomLevel, setMapPosition } = useCtxMap();
+  const queryClient = useQueryClient();
 
-  const { settings, isLoading } = useSettings();
+  const cachedSettings = queryClient.getQueryData<SettingsType>(["settings"]);
 
   useEffect(() => {
-    if (!settings || isLoading) return;
-    map.setView(settings?.defaultPosition, settings?.initialZoom, {
-      animate: true,
-    });
-    setZoomLevel(settings?.initialZoom);
-    setMapPosition(settings?.defaultPosition);
-  }, [isLoading, map, setMapPosition, setZoomLevel, settings]);
+    if (!cachedSettings) return;
+    map.setView(
+      cachedSettings?.data.defaultPosition,
+      cachedSettings?.data.initialZoom,
+      {
+        animate: true,
+      }
+    );
+    setZoomLevel(cachedSettings?.data.initialZoom);
+    setMapPosition(cachedSettings?.data.defaultPosition);
+  }, [cachedSettings, map, setMapPosition, setZoomLevel]);
 
   return null;
 };
