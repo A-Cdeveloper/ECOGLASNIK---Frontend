735f4e17a937530457e13a4de15f6708ca7153e3
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-18 00:25:52 +0100

    getSettingsApi function fix

--- src/features/settings/api/settings.ts
+++ src/features/settings/api/settings.ts
@@ -5,7 +5,7 @@ import { throwError } from "../../../utils/helpers";
 export const getSettingsApi = async (): Promise<SettingsType> => {
   try {
     const response = await fetch(`${API_URL}/settings`);
-    const { data } = await response.json();
+    const data = await response.json();
     if (!response.ok) {
       throw new Error(data.error);
     }
