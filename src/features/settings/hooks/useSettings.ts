080c7de21293af6f8f740bd81713f396cec4d63f
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-18 00:26:08 +0100

    useSettings fix

--- src/features/settings/hooks/useSettings.ts
+++ src/features/settings/hooks/useSettings.ts
@@ -3,14 +3,10 @@ import { SettingsType } from "../../../types";
 import { getSettingsApi } from "../api/settings";
 
 export const useSettings = () => {
-  const {
-    data: settings,
-    error,
-    isLoading,
-  } = useQuery<SettingsType>({
+  const { data, error, isLoading } = useQuery<SettingsType>({
     queryKey: ["settings"],
     queryFn: getSettingsApi,
   });
 
-  return { settings, error, isLoading };
+  return { settings: data, error, isLoading };
 };
