c7025a92118c3d10b5fc56eeaa9ccbedba704805
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-18 01:23:25 +0100

    change iconAnchor

--- src/features/map/CustumMarkerIcon.tsx
+++ src/features/map/CustumMarkerIcon.tsx
@@ -7,7 +7,7 @@ const CustumMarkerIcon = (status: string, activeMarker: boolean = false) => {
   const customIcon = new L.Icon({
     iconUrl: status === "done" ? solvedIcon : unsolvedIcon,
     iconSize: activeMarker ? [30, 30] : [30, 30],
-    iconAnchor: activeMarker ? [30, 15] : [30, 15],
+    iconAnchor: activeMarker ? [15, 15] : [15, 15],
   });
 
   return customIcon;
