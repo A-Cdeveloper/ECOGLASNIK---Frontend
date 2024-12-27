4209c99f077b4acc02f0bad6545776fb16d64a84
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-27 02:46:08 +0100

    new axios instance weatherClient

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
-import { API_URL } from "../constants";
+import { API_URL, WEATHER_API_KEY, WEATHER_BASE_URL } from "../constants";
 
 const apiClient = axios.create({
   baseURL: API_URL, // Base URL for all requests
   timeout: 5000, // Optional: Set a timeout for requests (in ms)
   headers: {
     "Content-Type": "application/json", // Default headers
+  },
+});
+
+export const weatherClient = axios.create({
+  baseURL: WEATHER_BASE_URL, // Centralized base URL
+  timeout: 5000, // Timeout for all requests
+  headers: {
+    "Content-Type": "application/json",
+  },
+  params: {
+    key: WEATHER_API_KEY, // Automatically include the API key in all requests
   },
 });
 
