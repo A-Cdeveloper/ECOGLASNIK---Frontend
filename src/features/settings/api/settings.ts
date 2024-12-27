060b24d83fce5ede5e56fb06bf32367d190b4a9a
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-27 02:20:42 +0100

    convert all fetch with axios in SETTINGS.ts

--- src/features/settings/api/settings.ts
+++ src/features/settings/api/settings.ts
@@ -1,3 +1,4 @@
+import { API_URL } from "../../../constants";
 import { SettingsType } from "../../../types";
 import apiClient from "../../../utils/axios";
 import { throwError } from "../../../utils/helpers";
@@ -5,7 +6,7 @@ import { throwError } from "../../../utils/helpers";
 export const getSettingsApi = async (): Promise<SettingsType> => {
   try {
     const response = await apiClient.get("/settings");
-    return response.data.data;
+    return response.data;
   } catch (error) {
     return await throwError(error);
   }
