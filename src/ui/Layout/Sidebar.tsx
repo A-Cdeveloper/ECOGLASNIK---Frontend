6d2aee2f66c0755423254923b0bb1cc9723e8089
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-01 13:06:51 +0100

    fix list problems padding bug

--- src/ui/Layout/Sidebar.tsx
+++ src/ui/Layout/Sidebar.tsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 const Sidebar = () => {
   return (
     <aside
-      className="w-full lg:w-1/2 xl:w-[450px] h-auto lg:h-screen border-e border-secondary-500/50 p-3 
-    overflow-auto pb-[30px] lg:pb-[100px] order-2 lg:order-1 custum-scrollbar mt-0 lg:mt-[70px]"
+      className="w-full lg:w-1/2 xl:w-[450px] h-auto lg:h-[90vh] border-e border-secondary-500/50 p-3 
+    overflow-auto pb-[30px] lg:pb-[100px] order-2 lg:order-1 custum-scrollbar mt-0 lg:mt-[70px] bg-red-600"
     >
       <Outlet />
     </aside>
