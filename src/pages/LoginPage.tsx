9be339efbf7842fd03983114445c016aae1612f4
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-04 00:45:22 +0100

    fix registration form problem height area

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
     return <Navigate to="/" replace />;
   }
   return (
-    <div className="w-full h-[80%] flex flex-col items-center justify-center px-2 sm:px-5 md:px-9">
+    <div className="w-full h-[100%] flex flex-col items-center justify-center px-2 sm:px-5 md:px-9">
       <LoginRegisterForm mode={mode} />
     </div>
   );
