0c909ea845637935c956f40e95c8c989037ce818
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-06 03:40:53 +0100

    add extra warrning when add problem to avoid problem duplication

--- src/features/problems/AddProblemBanner.tsx
+++ src/features/problems/AddProblemBanner.tsx
@@ -5,10 +5,14 @@ const AddProblemBanner = () => {
   const { error } = useGeolocation();
 
   return (
-    <div className="flex flex-col items-center  h-auto space-y-5">
-      <div className=" bg-yellow-500/80 px-2 py-2 text-center text-[15px]/[1.25]">
+    <div className="flex flex-col items-center  h-auto space-y-2">
+      <div className=" bg-warrning-500/60 px-2 py-2 text-center text-[15px]/[1.25]">
         Kliknite na mapu kako biste odabrali lokaciju problema koji
         prijavljujete ili odaberite Vašu trenutnu lokaciju.
+      </div>
+      <div className=" bg-danger-500/60 px-2 py-2 text-[15px]/[1.25]">
+        Molimo Vas da pre prijave komunalnog problema proverite da li je problem
+        već prijavljen. Hvala.
       </div>
       {error && <AuthNotification state="error" message={error} />}
     </div>
