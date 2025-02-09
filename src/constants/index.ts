7c36d44a0abcf4b0328fe784c960325795b29d30
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-09 13:35:29 +0100

    delete user checng fetch with axios

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
-//export const API_URL = import.meta.env.VITE_API_URL;
 export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://clean-me-backend.vercel.app/api"
--- src/features/users/api/users.ts
+++ src/features/users/api/users.ts
@@ -1,22 +1,13 @@
-import { API_URL } from "../../../constants";
 import { User } from "../../../types";
+import apiClient from "../../../utils/axios";
 import { throwError } from "../../../utils/helpers";
 
 /// get user from session storage
 
 export const deleteUserApi = async (uid: number): Promise<User> => {
   try {
-    const response = await fetch(`${API_URL}/users/${uid}`, {
-      method: "DELETE",
-      credentials: "include",
-    });
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(data.error);
-    }
-
-    return data;
+    const response = await apiClient.delete(`/users/${uid}`);
+    return response.data;
   } catch (error) {
     return await throwError(error);
   }
