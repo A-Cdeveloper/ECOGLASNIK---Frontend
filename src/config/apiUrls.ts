8de1a80181c08fda1551dee196aaaefb9206d600
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-20 01:13:03 +0100

    change config

--- src/config/apiUrls.ts
+++
@@ -1,1 +0,0 @@
-export const apiUrls = ["demo", "blank", "vlasotince", "leskovac"];
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,3 @@
-import { apiUrls } from "./apiUrls";
-
 export const APP_NAME = "ECOGLASNIK";
 
 // Forms
@@ -12,20 +10,4 @@ export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 // API
 
-const getApiBaseUrl = () => {
-  if (typeof window === "undefined")
-    return "https://www.demo-api.ecoglasnik.org/api";
-
-  const hostname = window.location.hostname;
-
-  if (hostname.includes("localhost")) {
-    return "http://localhost:3000/api"; // Local API
-  }
-
-  const matchedUrl = apiUrls.find((url) => hostname.includes(url));
-  return matchedUrl
-    ? `https://www.${matchedUrl}-api.ecoglasnik.org/api`
-    : "https://www.demo-api.ecoglasnik.org/api"; // Default to demo API
-};
-
-export const API_URL = getApiBaseUrl();
+export const API_URL = import.meta.env.VITE_API_URL;
