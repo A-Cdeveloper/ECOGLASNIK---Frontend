7edee0926dd03dab0ef9fd677d2ab889adc9bbbf
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-24 17:53:47 +0100

    remove useContext hook

--- src/context/mapContext.tsx
+++ src/context/mapContext.tsx
@@ -1,5 +1,5 @@
 import type { Map } from "leaflet";
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 import { Position } from "../types";
 
 type MapState = {
